475b8d8e3418ecaaf3cacce8fb7ebdd781b3614d
Ken Bannister <kb2ma@runbox.com>
2021-12-11 18:46:13 -0500

    Add provisioning to AWS.
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
+import fetch from 'node-fetch'
 
 /**
- * Verify that expected environment variables from registration exist.
+ * Verify that *all* expected environment variables from registration exist.
  */
-function isRegistered() {
-    if (process.env.AWS_PRIVATE_KEY
+function isRegistrationComplete() {
+    return process.env.AWS_PRIVATE_KEY
             && process.env.AWS_CERT
-            && process.env.AWS_ROOT_CA) {
-        console.log('Cloud environment variables found.')
-        return true
-    }
-    console.log('Cloud environment variables not found.')
-    return false
+            && process.env.AWS_ROOT_CA
 }
 
-async function register() {
+/**
+ * Verify that *none* of the expected environment variables from registration exist.
+ */
+function isUnregistered() {
+    return !process.env.AWS_PRIVATE_KEY
+            && !process.env.AWS_CERT
+            && !process.env.AWS_ROOT_CA
+}
+
+/**
+ * Provision provided device to cloud. Throws on HTTP error response code.
+ */
+async function provision(uuid) {
+    let url = process.env.PROVISION_URL
+    if (!url) {
+        throw 'PROVISION_URL environment variable not defined'
+    }
+    
+    const response = await fetch(url, {
+        method: 'post',
+        body: `{ "uuid": "${uuid}", "attributes": {} }`,
+        headers: {'Cache-Control': 'no-cache', 'Content-Type': 'application/json'}
+    })
+    if (response.ok) {
+        // response.status >= 200 && response.status < 300
+        console.log(`Provisioned OK: ${response.status}`)
+    } else {
+        throw `Provisioning failure: ${response.status}, ${response.statusText}`;
+    }
 }
 
 async function start() {
-    if (!isRegistered()) {
-        await register()
-    } else {
-        //await connect()
+    try {
+        if (isUnregistered()) {
+            await provision(process.env.RESIN_DEVICE_UUID)
+        } else if (isRegistrationComplete()) {
+            //await connect()
+        } else {
+            console.log('Partially registered; try again later')
+        }
+    } catch(e) {
+        console.error(e)
     }
 }
 

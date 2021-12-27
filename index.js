3a585812a348ce705278fc530ae814ce3d649434
Ken Bannister <kb2ma@runbox.com>
2021-12-27 12:41:57 -0500

    Move messaging to separate library file
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import fetch from 'node-fetch'
 import mqtt from 'async-mqtt'
 import awsIot from 'aws-iot-device-sdk'
+import Messenger from './lib/messenger.js'
 // just for debugging with util.inspect, etc.
 //import util from 'util'
 
 // async wrapper for MQTT client
 let localMqtt = null
-// AWS IoT wrapper for MQTT client (not async)
-let awsMqtt = null
+// messenger with cloud provider (not async)
+let cloudMsgr = null
 
 /**
  * Verify that *all* expected environment variables from registration exist.
@@ -60,19 +61,14 @@ async function connectLocal() {
     await localMqtt.subscribe(process.env.PRODUCER_TOPIC, { qos: 1 })
 }
 
-/** Connects to cloud provider MQTT. */
+/** Connects to cloud provider'S (MQTT) messaging. */
 function connectCloud() {
     if (!process.env.CLOUD_DATA_TOPIC) {
         process.env.CLOUD_DATA_TOPIC = 'sensors'
     }
 
-    awsMqtt = awsIot.device({
-        privateKey: Buffer.from(process.env.AWS_PRIVATE_KEY, 'base64'),
-        clientCert: Buffer.from(process.env.AWS_CERT, 'base64'),
-            caCert: Buffer.from(process.env.AWS_ROOT_CA, 'base64'),
-          clientId: process.env.RESIN_DEVICE_UUID,
-              host: process.env.AWS_DATA_ENDPOINT,
-        })
+    cloudMsgr = Messenger.create()
+    cloudMsgr.connect()
 }
 
 /** Runs the relay. Wraps all execution in a try/catch block. */
@@ -86,8 +82,7 @@ async function start() {
             connectCloud()
 
             localMqtt.on('message', function (topic, message) {
-                //console.log(message.toString())
-                awsMqtt.publish(process.env.CLOUD_DATA_TOPIC, message)
+                cloudMsgr.publish(process.env.CLOUD_DATA_TOPIC, message)
             })
 
         } else {

31a48d74954aaf177a31b0584049bde44d161dfb
Ken Bannister <kb2ma@runbox.com>
2022-01-08 20:08:50 -0500

    Improve error handling for GCP and local MQTT
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- index.js
+++ index.js
@@ -11,36 +11,91 @@ let localMqtt = null
 let cloudMsgr = null
 
 /**
- * Provision provided device to cloud. Throws on HTTP error response code.
+ * Provision provided device to cloud.
+ * 
+ * @return {boolean} true if provisioning successful; otherwise false.
  */
 async function provision(uuid) {
     let url = process.env.PROVISION_URL
     if (!url) {
         throw 'PROVISION_URL environment variable not defined'
     }
-    
+    console.log("Provisioning with cloud provider")
+
     const response = await fetch(url, {
-        method: 'post',
+        method: 'POST',
         body: `{ "uuid": "${uuid}", "attributes": {} }`,
         headers: {'Cache-Control': 'no-cache', 'Content-Type': 'application/json'}
     })
+    const text = await response.text()
     if (response.ok) {
         // response.status >= 200 && response.status < 300
-        console.log(`Provisioned OK: ${response.status}`)
+        console.log(`Provisioned OK: ${response.status} ${text}`)
     } else {
-        throw `Provisioning failure: ${response.status}, ${response.statusText}`;
+        console.warn(`Provisioning failure: ${response.status} ${text}`)
+
+        // GCP error handling:
+        // If device already provisioned, Supervisor may not have updated environment
+        // vars yet and thus tried to provision again. So force Supervisor to update
+        // and refresh environment variables. If successful, this service will
+        // not attempt to provision on the next invocation.
+        if (process.env.CLOUD_PROVIDER == 'GCP') {
+            let respJson = {}
+            try {
+                respJson = JSON.parse(text)
+            } catch(e) {
+                // just use empty respJson
+            }
+            const alreadyExistsCode = 6
+
+            if (respJson.code && respJson.code == alreadyExistsCode) {
+                console.warn("GCP device already exists; updating Supervisor to refresh environment vars")
+                const updateUrl = `${process.env.BALENA_SUPERVISOR_ADDRESS}/v1/update?apikey=${process.env.BALENA_SUPERVISOR_API_KEY}`
+                const updateResp = await fetch(updateUrl, {
+                    method: 'POST',
+                    body: '{ "force": true }',
+                    headers: { 'Content-Type': 'application/json' }
+                })
+                console.log("Supervisor updated:", updateResp.status)
+            }
+        }
     }
+    return response.ok
 }
 
-/** Connects and subscribes to local MQTT topic. */
+/**
+ * Connects and subscribes to local MQTT topic. Retries twice if can't connect.
+ *
+ * If success, 'localMqtt' is not null.
+ */
 async function connectLocal() {
     if (!process.env.PRODUCER_TOPIC) {
         process.env.PRODUCER_TOPIC = 'sensors'
     }
 
-    localMqtt = await mqtt.connectAsync('mqtt://127.0.0.1')
-    console.log("Connected to mqtt://127.0.0.1")
-    await localMqtt.subscribe(process.env.PRODUCER_TOPIC, { qos: 1 })
+    let count = 0
+    const delay = 5
+    let isConnected = false
+    do { 
+        try {
+            if (!localMqtt) {
+                localMqtt = await mqtt.connectAsync('mqtt://127.0.0.1')
+                console.log("Connected to mqtt://127.0.0.1")
+            }
+            await localMqtt.subscribe(process.env.PRODUCER_TOPIC, { qos: 1 })
+            console.log("Subscribed to topic:", process.env.PRODUCER_TOPIC)
+            isConnected = true
+        } catch(e) {
+            console.warn("Cannot connect to local MQTT:", e)
+            count++
+            if (count == 3) {
+                localMqtt = null
+                break
+            }
+            console.log(`Retry in ${delay} seconds`)
+            await new Promise(r => setTimeout(r, delay * 1000))
+        }
+    } while(!isConnected)
 }
 
 /** Runs the relay. Wraps all execution in a try/catch block. */
@@ -58,18 +113,20 @@ async function start() {
 
         } else if (cloudMsgr.isRegistrationComplete()) {
             await connectLocal()
-            if (!process.env.CLOUD_DATA_TOPIC) {
-                process.env.CLOUD_DATA_TOPIC = cloudMsgr.defaultDataTopic
-            }
-            if (cloudMsgr.isSyncConnect()) {
-                cloudMsgr.connectSync()
-            } else {
-                await cloudMsgr.connect()
-            }
+            if (localMqtt) {
+                if (!process.env.CLOUD_DATA_TOPIC) {
+                    process.env.CLOUD_DATA_TOPIC = cloudMsgr.defaultDataTopic
+                }
+                if (cloudMsgr.isSyncConnect()) {
+                    cloudMsgr.connectSync()
+                } else {
+                    await cloudMsgr.connect()
+                }
 
-            localMqtt.on('message', function (topic, message) {
-                cloudMsgr.publish(process.env.CLOUD_DATA_TOPIC, message)
-            })
+                localMqtt.on('message', function (topic, message) {
+                    cloudMsgr.publish(process.env.CLOUD_DATA_TOPIC, message)
+                })
+            }
 
         } else {
             console.log('Partially registered; try again later')
